ea4d021b0a06007a885c6e1741cae4a1160e4a69
reganl <reganl@sfu.ca>
2020-12-14 12:05:07 -0800

    Added landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,21 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
+import logo from '../logo.svg';
+import '../App.css';
+
 const Landing = () => {
   return (
     <Fragment>
-      <h1>Home Inspection Checklist</h1>
-
-      <Link to='/checklist'>Submit Checklist</Link>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p className='font-weight-light'>Home Inspection Checklist</p>
+          <Link to='/checklist' className='btn btn-outline-info'>
+            Submit Checklist
+          </Link>
+        </header>
+      </div>
     </Fragment>
   );
 };
